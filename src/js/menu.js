3694ab67b845f6c50e36d7c3da45204c325efa72
Olga Zak <olgaliam@gmail.com>
2022-07-16 00:52:01 +0300

    mobile menu is done
    added js to close menu by link click

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,7 +5,7 @@
       menu: document.querySelector('[data-menu]'),
     };
     refs.openMenuBtn.addEventListener('click', toggleModal);
-    refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.closeMenuBtn.addEventListener('click', toggleModal);
     function toggleModal() {
       refs.menu.classList.toggle('is-open');
     }
