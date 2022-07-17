abb623976302a9dc7c96ea8b219cbfa18749f845
Emil-Bayramov <105307406+Emil-Bayramov@users.noreply.github.com>
2022-07-17 20:14:17 +0000

    Deploying to gh-pages from @ Emil-Bayramov/not-last-child@514aa0a40fda13ab00c8d6313ab4cb193ad31d37 🚀

---
+++ index.76a9f1b8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-franchise]"),closeModalBtn:document.querySelector("[data-modal-close-franchise]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.76a9f1b8.js.map
